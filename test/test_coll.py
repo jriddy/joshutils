7fcdc1a6528858f20802560491fb9ded3f47ac6c
Josh Reed <jreed@ok7-eleven.com>
2016-09-21 15:35:58 -0500

    expand chunk unit tests

--- test/test_coll.py
+++ test/test_coll.py
@@ -7,3 +7,9 @@ class TestChunk(unittest.TestCase):
 
     def test_even(self):
         self.assertEqual(coll.chunk(range(4), 2), [(0, 1), (2, 3)])
+
+    def test_odd(self):
+        self.assertEqual(coll.chunk(range(5), 2), [(0, 1), (2, 3)])
+
+    def test_type_error(self):
+        self.assertRaises(TypeError, coll.chunk, range(10), 1.0)
